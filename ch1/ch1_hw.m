44a7614262025cec4c1da94fb8ba814abe9f67c4
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2025-09-19 20:06:43 -0700

    add chapter 1 notes and hw

---
+++ ch1/ch1_hw.m
@@ -0,0 +1,90 @@
+%% Math 320 - Chapter 1 Homework
+% Tomas Oh
+
+close all, clear, clc, format compact
+%% Chapter 1, Problem #2
+
+%Part (a)
+a = (8 + 80/2.6)^(1/3) + exp(3.5)
+
+%Part (b)
+b = (1/sqrt(75) + 73/(3.1^3))^(1/4) + 55*0.41
+
+%% Chapter 1, Problem #5
+
+%Part (a)
+a = sin(0.2*pi) / cos(pi/6) + tand(72)
+
+%Part (b)
+b = (tand(64) * cosd(15))^2 + (sind(37)^2 / cosd(20)^2)
+
+%% Chapter 1, Problem #6
+
+z = 4.5;
+
+%Part (a)
+a = 0.4*z^4 + 3.1*z^2 - 162.3*z - 80.7
+
+%Part (b)
+b = (z^3 - 23) / ((z^2 + 17.5)^(1/3))
+
+%% Chapter 1, Problem #9
+
+%Part (a)
+c = 4.6; d = 1.7; a = c*d^2; b = (c+a)/(c-d);
+a = exp(d - b) + (c + a)^(1/3) - (c*a)^d
+
+%Part (b)
+t = 1;
+b = d/c + (c*t/b)^2 - c^d - a/b
+
+%% Chapter 1, Problem #16
+
+%Part (a)
+r1 = 15; r2 = 10.5; r3 = 4.5;
+a = r2 + r3; b = r1 + r3; c = r1 + r2;
+gamma = acosd((c ^ 2 - a ^ 2 - b ^ 2) / (a * b * (-2)))
+
+%Part (b)
+alpha = asind(a*(sind(gamma) / c))
+beta = asind(b*(sind(gamma) / c))
+
+%Part (c)
+sum = alpha + beta + gamma
+
+%% Chapter 1, Problem #21
+
+a = 18; b = 7;
+P = pi*(a + b)*(3 - (sqrt((3*a + b)*(a + 3*b)))/(a + b))
+
+%% Chapter 1, Problem #28
+
+%Part (a)
+n = 26; r = 6;
+P = factorial(n) / factorial(n - r)
+
+%Part (b)
+n = 36;
+P = factorial(n) / factorial(n - r)
+
+%% Chapter 1, Problem #32
+
+k = log(0.5) / 5730;
+t = round(log(0.7745) / k)
+
+%% Chapter 1, Problem #34
+
+M = 9.2;
+
+%Part (a)
+E = 1.74 * 10^19 *10^(1.44*M)
+
+%Part (b)
+M = log10(E / (2*1.74 * 10^19)) / 1.44
+
+%% Chapter 1, Problem #39
+
+n = 19;
+approxVal = sqrt((2*n + 1/3)*pi) * n^n * exp(-n)
+trueVal = factorial(n);
+Error = (trueVal - approxVal) / trueVal
--- ch1/ch1_review.m
+++ ch1/ch1_review.m
@@ -1,2 +1,80 @@
 %% Chapter 1 Review
 
+%% MATLAB Basics
+
+%{
+Command Window - used to:
+- Execute commands
+- Open other windows
+- Run programs written by the user
+- Manage the MATLAB software
+
+Notes about using the Command Window:
+- If command too long, can use ... then Enter
+- Semicolon (;) suppresses the output
+- Percent sign (%) treated as comments
+- clc clears the command window display
+%}
+
+%% Arithmetic Operations with Scalars
+
+%{
++, -, *, /, \, ^ are all supported (follows PEMDAS)
+%}
+
+%% Display Formats
+
+%{
+format short -      4 digits after decimal point
+format long -       15 digits after decimal point
+format short e -    Scientific notation with 4 digits after decimal
+format long e -     Scientific notation with 15 digits after decimal
+format short g -    Fixed or scientifc, whichever more compact (5 dig)
+format long g -     Fixed or scientifc, whichever more compact (15 dig)
+format bank -       2 digits after decimal
+format rational -   Ratio of small integers
+format compact -    Suppresses excess blank lines for more screen output
+format loose -      Adds more blank lines in output
+format default -    Back to defaults (format short, format loose)
+%}
+
+%% Elementary Math Built-in Functions
+
+%{
+Some MATLAB built-in functions:
+- sqrt(x), nthroot(x, n), exp(x), abs(x), log(x), log10(x), factorial(x)
+- pi for 𝜋
+%}
+
+%{
+Trigonometric Math functions:
+- sin(x), cos(x), tan(x), cot(x), asin(x), ...
+- Note: put at d at the end of the trigonometric function for degrees
+    - sind(x), cosd(x), ...
+%}
+
+%% Rounding Functions
+
+%{
+- round(x) or round(x, n) for n digits
+- fix(x) towards 0, ceil(x) towards inf, floor(x) towards -inf
+- rem(x, y) returns the remainder of x/y
+- sign(x) returns 1 if positive, 0 if 0, -1 if negative
+%}
+
+%% Defining Scalar Variables
+
+%{
+my_var = numeric value or computable expression
+Rules:
+- Must begin with a letter, up to 63 characters long, case-sensitive
+- Can contain letters, digits, and underscore
+- Can't be the name of a built-in function or keyword
+%}
+
+%{
+Commands for Managing Variables:
+- clear (removes variables from memory) or clear x y (clears x y)
+- who (displays list of variables in memory)
+- whos (displays list of variables in memory with size info.)
+%}
